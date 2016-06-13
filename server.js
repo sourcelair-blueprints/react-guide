4412d94bd1cca199b99782d99b7c7c64af4a233f
Mitch <mitch.karajohn@gmail.com>
2016-06-13 20:11:30 +0300

    Updated server.js and added body-parser to deps

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-var express = require('express');
+var fs = require('fs');
 var path = require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
-
 app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, function () {
+  console.log('React app listening on port 3000');
+});
